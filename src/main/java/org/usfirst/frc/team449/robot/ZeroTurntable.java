aa80a61c4e5e2a3f33b15b478b69fd17c96bc528
noah.gleason <noah.gleason1@gmail.com>
2016-11-05 16:08:17 -0400

    Added position caliration on startup.

--- src/main/java/org/usfirst/frc/team449/robot/ZeroTurntable.java
+++ src/main/java/org/usfirst/frc/team449/robot/ZeroTurntable.java
@@ -1,7 +1,29 @@
 package org.usfirst.frc.team449.robot;
 
+import edu.wpi.first.wpilibj.CANTalon;
+
 /**
  * Created by BlairRobot on 2016-11-05.
  */
-public class ZeroTurntable {
+public class ZeroTurntable extends ReferencingCommand {
+    private CANTalon canTalon;
+
+    public ZeroTurntable(MappedSubsystem subsystem, double timeout, CANTalon canTalon) {
+        super(subsystem, timeout);
+        requires(subsystem);
+        this.canTalon = canTalon;
+        System.out.println("ZeroTurntable constructed");
+    }
+
+    protected void initialize() {
+        canTalon.set(.5);
+    }
+
+    protected boolean isFinished() {
+        return canTalon.isFwdLimitSwitchClosed();
+    }
+
+    protected void end() {
+        canTalon.set(0);
+    }
 }
