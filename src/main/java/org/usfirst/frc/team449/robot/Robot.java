13e5f68bc8604af48ddb4ec88a489d1b8668ab4a
Ryan Tse <rytse@mbhs.edu>
2016-12-10 10:08:04 -0500

    Test shooter after changes; it works

--- src/main/java/org/usfirst/frc/team449/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team449/robot/Robot.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team449.robot;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import org.json.JSONObject;
+import org.usfirst.frc.team449.robot.mechanism.shooter.ShooterMap;
 import org.usfirst.frc.team449.robot.mechanism.shooter.ShooterSubsystem;
 import org.usfirst.frc.team449.robot.mechanism.turntable.TurntableMap;
 import org.usfirst.frc.team449.robot.mechanism.turntable.TurntableSubsystem;
@@ -43,10 +44,10 @@ public class Robot extends IterativeRobot {
 		cfg = MappedSubsystem.readConfig("/home/lvuser/cfg.json");
 		System.out.println("Start map init");
 		oi = new TurretOI(new OIMap(cfg));
-		System.out.println("Start turntable init");
-		turntableSubsystem = new TurntableSubsystem(new TurntableMap(cfg), oi);
-		//System.out.println("Start shooter init");
-		//shooterSubsystem = new ShooterSubsystem(new ShooterMap(cfg), oi);
+//		System.out.println("Start turntable init");
+//		turntableSubsystem = new TurntableSubsystem(new TurntableMap(cfg), oi);
+		System.out.println("Start shooter init");
+		shooterSubsystem = new ShooterSubsystem(new ShooterMap(cfg), oi);
 
 		// Map buttons AFTER all the subsystems are inited
 		oi.mapButtons();
